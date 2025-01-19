ead84b8bbf923224fc8d4f0e51582a9cd14015e1
P0r43vo <igor.limno@icloud.com>
2025-01-19 16:19:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+            string UpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string Numbers = "0123456789";
+            string SpecialCharacters = "!?-_@#$%^&*/";
+            string AllTogether = LowerCaseCharacters + UpperCaseCharacters + Numbers + SpecialCharacters;
+            
+            int PasswordLength = 8;
+
+            Random random = new Random();
+            char[] PasswordArray = new char[PasswordLength];
+            for (int i = 0; i < PasswordLength; i++)
+            {
+                PasswordArray[i] = AllTogether[random.Next(AllTogether.Length)];
+            }
+            string password = new string(PasswordArray);
+            Console.WriteLine("Here is your password:");
+            Console.WriteLine(password);
+            
+            bool HasLowerCase = password.Any(char.IsLower);
+            bool HasUpperCase = password.Any(char.IsUpper);
+            bool HasNumbers = password.Any(char.IsDigit);
+            bool HasSpecialCharacters = password.Any(ch => char.IsLetterOrDigit(ch));
+            
+            if (HasLowerCase && HasUpperCase && HasNumbers && HasSpecialCharacters)
+            {
+                Console.WriteLine("Your password is strong");
+            }
+            else
+            {
+                Console.WriteLine("Your password is weak");
+            }
+
+        }
+    }
+}
