24286d52853296867da59695f93844ff215a0b0f
P0r43vo <igor.limno@icloud.com>
2025-01-27 22:48:23 +0100

    homework for 28/01/25

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,11 @@ namespace ConsoleApp
     {
         static void Main()
         {
+            Main3();
+        }
+
+        static void Main1()
+        {
             string LowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
             string UpperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string Numbers = "0123456789";
@@ -51,6 +56,59 @@ namespace ConsoleApp
                 Console.WriteLine("Your password should contain special character");
             }
 
+        }
+
+        static void Main2()
+        {
+                Console.WriteLine("Please enter your percentage score: ");
+                string PercentageScore = Console.ReadLine();
+                int Score = int.Parse(PercentageScore);
+                
+                if (0 <= Score && Score <= 39)
+                {
+                    Console.WriteLine("You have failed your test ");
+                }
+                else if (40 <= Score && Score <= 59)
+                {
+                    Console.WriteLine("You have passed your test ");
+                }
+                else if (60 <= Score && Score <= 79)
+                {
+                    Console.WriteLine("Your test score is merit");
+                }
+                else if (80 <= Score && Score <= 100)
+                {
+                    Console.WriteLine("Your test score is distinctive");
+                }
+                else if (0 > Score || Score > 100)
+                {
+                    Console.WriteLine("Invalid percentage");
+                }
+                else if (!int.TryParse(Score.ToString(), out Score))
+                {
+                    Console.WriteLine("Not a number");
+                }
+        }
+
+        static void Main3()
+        {
+            Console.WriteLine("Please write your forename");
+            string Forename = Console.ReadLine();
+            
+            Console.WriteLine("Please write your surnname");
+            string Surname = Console.ReadLine();
+            
+            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int NumberGen;
+            
+            Random rand = new Random();
+
+            NumberGen = rand.Next(0, numbers.Length) + rand.Next(0, numbers.Length) + rand.Next(0, numbers.Length);
+            string NG = NumberGen.ToString();
+
+            string idcode = Forename[0] + Forename[1] + Surname[0] + Surname[^1].ToString() + NG;
+            
+            Console.WriteLine("Your student id is" + idcode);
         }
     }
 }
